aa992e20db1e5a01bbc1907df8350b6624d95d72
Melina <Melina@LAPTOP-T2L8DC7G>
2018-12-13 11:15:42 +0100

    Plus de bug mais pas d'affichage de la page web

--- src/ch/makery/Controller.java
+++ src/ch/makery/Controller.java
@@ -3,6 +3,7 @@ package ch.makery;
 import java.io.IOException;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
@@ -14,7 +15,9 @@ import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 public class Controller {
+	@FXML
 	private WebView WebViewer;
+
 	
 	public void LienMagazine(ActionEvent event) throws IOException{
 			
@@ -62,21 +65,19 @@ public class Controller {
 		//LienArticle("https://edition.cnn.com/2018/12/12/sport/hines-ward-nfl-reflections-after-week-14-spt-intl/index.html", event);
 	}
 	
+	@FXML
 	public void LienArticle(ActionEvent event) throws IOException{
-		WebView WebViewer = new WebView();
-		WebEngine engine = WebViewer.getEngine();
-		engine.load("https://edition.cnn.com/2018/12/12/sport/hines-ward-nfl-reflections-after-week-14-spt-intl/index.html");
-		
-		Pane root = new Pane();
-		root.getChildren().addAll(WebViewer);
-		
+		WebView myWebView = new WebView();
+		WebEngine engine = myWebView.getEngine();
+		engine.load("https://www.google.com");
+		WebViewer = myWebView;
 		Parent tableViewParent=FXMLLoader.load(getClass().getResource("view/Article.fxml"));
-		
+		Pane root = new Pane();
+		root.getChildren().addAll(myWebView);
 		
 		Scene tableViewScene =new  Scene(tableViewParent);
 			
 		Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
-	
 		window.setScene(tableViewScene);
 		window.show();
 		
