7dded99ff265ef0446d71f2341eed01bfbaea937
Melina <Melina@LAPTOP-T2L8DC7G>
2018-12-13 08:49:55 +0100

    Avancement des magazines, mais pb sur le webview

--- src/ch/makery/Controller.java
+++ src/ch/makery/Controller.java
@@ -7,9 +7,14 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
 import javafx.stage.Stage;
 
 public class Controller {
+	private WebView WebViewer;
+	
 	public void LienMagazine(ActionEvent event) throws IOException{
 			
 		Parent tableViewParent=FXMLLoader.load(getClass().getResource("view/Magazines.fxml"));
@@ -49,5 +54,34 @@ public class Controller {
 	
 		window.setScene(tableViewScene);
 		window.show();
+	}
+	
+	public void ArticleSport2(ActionEvent event) throws IOException{
+		//LienArticle("https://edition.cnn.com/2018/12/12/sport/hines-ward-nfl-reflections-after-week-14-spt-intl/index.html", event);
+	}
+	
+	public void LienArticle(ActionEvent event) throws IOException{
+		WebView WebViewer = new WebView();
+		WebEngine engine = WebViewer.getEngine();
+		engine.load("https://edition.cnn.com/2018/12/12/sport/hines-ward-nfl-reflections-after-week-14-spt-intl/index.html");
+		
+		Pane root = new Pane();
+		root.getChildren().addAll(WebViewer);
+		
+		Parent tableViewParent=FXMLLoader.load(getClass().getResource("view/Article.fxml"));
+		
+		
+		Scene tableViewScene =new  Scene(tableViewParent);
+			
+		Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+	
+		window.setScene(tableViewScene);
+		window.show();
+		
+	}
+	
+	public void GererWebView(String lien) {
+		
+		
 	}
 }
